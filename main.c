a042d26b6e430f81e305ae8db30b9172cd312308
SergeyVolik <volik.sergey112@gmail.com>
2024-01-27 18:27:31 +0200

    :sparkles: glfw window

--- main.c
+++ main.c
@@ -10,28 +10,73 @@
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-#include <stdio.h>
-#include <time.h>
-#include "header.h"
+//#include <stdio.h>
+//#include <time.h>
+//#include "header.h"
+//
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//
+//	game_init();
+//
+//	move_snake_t = move_tick_time;
+//	while (exit_app == 0)
+//	{
+//		input_read();
+//		app_update_time();
+//
+//		if (pause == 1)
+//			continue;
+//
+//		game_update();
+//	}
+//
+//	delete_game_data();
+//	return 0;
+//}
 
-int main()
+#include <GLFW/glfw3.h>
+#include <windows.h>
+
+int main(void)
 {
-	srand((unsigned int)time(NULL));
+    GLFWwindow* window;
 
-	game_init();
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-	move_snake_t = move_tick_time;
-	while (exit_app == 0)
-	{
-		input_read();
-		app_update_time();
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Snake", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-		if (pause == 1)
-			continue;
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-		game_update();
-	}
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
 
-	delete_game_data();
-	return 0;
+        glBegin(GL_TRIANGLES);
+        glColor3f(255, 255, 255);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0, -0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
